973065ccaaf2df1238722062126473c46c149629
JUELY LAURENTINO MENEZES <94908054+Juely2021@users.noreply.github.com>
2022-09-24 14:59:55 -0300

    Criacao do css, processa.php, form e inputs

--- login.php
+++ login.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <title>Login</title>
 </head>
 <body>
     <h1>Login</h1>
+    <form action="processa.php" method="post">
+        <input type="email">
+        <input type="password">
+        <input type="submit">
+    </form>
 </body>
 </html>
